0d96855da65063ddd3d5d93dc496cfe4417765c5
emzubair <muhammad.zubair@arbisoft.com>
2018-11-06 16:59:33 +0500

    enabled django admin access and super user

--- src/profiles_project/profile_api/models.py
+++ src/profiles_project/profile_api/models.py
@@ -18,10 +18,10 @@ class UserProfileManager(BaseUserManager):
 
         return user
 
-    def create_super_user(self,email,name,password):
+    def create_superuser(self,email,name,password):
         """Creates and store a new Superuser with given Details."""
         user = self.create_user(email,name,password)
-        user.is_super_user = True
+        user.is_superuser = True
         user.is_staff = True
         user.save(using=self._db)
         return user
